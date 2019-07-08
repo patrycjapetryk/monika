3498c582c74d823f5834a1baa0a929989adaecef
Patrycja Petryk <patrycja.petryk@gmail.com>
2019-07-08 16:51:46 +0200

    mobile menu styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 "use strict";
 
-import moment from 'moment';
+// import moment from 'moment';
+// console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+const menuButton = document.querySelector('.navigation__button--js');
+const navigationItems = document.querySelectorAll('.navigation__item--js');
+
+menuButton.addEventListener('click', () => {
+    navigationItems.forEach(item => item.classList.toggle('navigation__item--active'));
+})

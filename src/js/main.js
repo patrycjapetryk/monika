bdeaf6af93fc604237ef1709500ddf3c13a01129
Patrycja Petryk <patrycja.petryk@gmail.com>
2019-07-16 15:55:54 +0200

    fix: mobile menu & scss structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 "use strict";
 
-// import moment from 'moment';
-// console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
-
 const menuButton = document.querySelector('.navigation__button--js');
 const navigationItems = document.querySelectorAll('.navigation__item--js');
 
 menuButton.addEventListener('click', () => {
-    navigationItems.forEach(item => item.classList.toggle('navigation__item--active'));
-})
+    navigationItems.forEach(item => {
+        item.classList.toggle('navigation__item--active');
+        item.classList.contains("navigation__item--active") ? menuButton.innerHTML = "close" : menuButton.innerHTML = "menu";
+    });
+});

9ef94de1691c7f91c81538871f8b8c173e8d7537
Patrycja Petryk <patrycja.petryk@gmail.com>
2019-09-26 11:29:41 +0200

    feat: add gallery menu active on scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,36 @@
 "use strict";
 
-const menuButton = document.querySelector('.navigation__button--js');
-const navigationItems = document.querySelectorAll('.navigation__item--js');
+const menuButton = document.querySelector(".navigation__button--js");
+const navigationItems = document.querySelectorAll(".navigation__item--js");
 
-menuButton.addEventListener('click', () => {
-    navigationItems.forEach(item => {
-        item.classList.toggle('navigation__item--active');
-        item.classList.contains("navigation__item--active") ? menuButton.innerHTML = "close" : menuButton.innerHTML = "menu";
-    });
+menuButton.addEventListener("click", () => {
+	navigationItems.forEach(item => {
+		item.classList.toggle("navigation__item--active");
+		item.classList.contains("navigation__item--active")
+			? (menuButton.innerHTML = "close")
+			: (menuButton.innerHTML = "menu");
+	});
 });
+
+const galleries = document.querySelectorAll(".gallery--js");
+const galleryNavigationItems = document.querySelectorAll(
+	".navigation__item--gallery-js"
+);
+
+const activeMenuItem = () => {
+	galleryNavigationItems.forEach((listItem, index) => {
+		const viewportOffsetTop = galleries[index].getBoundingClientRect().top;
+		const halfOfWindowHeight = window.innerHeight / 2;
+		const galleryHeight = galleries[index].clientHeight;
+		if (
+			viewportOffsetTop < halfOfWindowHeight &&
+			viewportOffsetTop > -1 * (galleryHeight - halfOfWindowHeight)
+		) {
+			listItem.querySelector("a").classList.add("navigation__link--active");
+		} else {
+			listItem.querySelector("a").classList.remove("navigation__link--active");
+		}
+	});
+};
+
+window.addEventListener("scroll", activeMenuItem);
